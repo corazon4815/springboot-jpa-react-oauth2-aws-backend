f663d3428aef621b346f76e62a266e32a0c5c982
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-10-19 22:36:19 +0900

    test : todo 수정, 삭제 test 코드 추가

--- src/test/java/com/web/springboot/domain/Todo/TodoRepositoryTest.java
+++ src/test/java/com/web/springboot/domain/Todo/TodoRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.web.springboot.domain.Todo;
 
+import com.web.springboot.common.exception.CustomException;
 import com.web.springboot.domain.TodoEntity;
 import com.web.springboot.domain.TodoRepository;
+import com.web.springboot.dto.TodoDTO;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +23,9 @@ public class TodoRepositoryTest {
         todoRepository.deleteAll();
     }
 
-    //투두 생성
+    /*
+     *    투두 저장
+     */
     @Test
     public void postTodo() {
         todoRepository.save(TodoEntity.builder()
@@ -31,9 +35,37 @@ public class TodoRepositoryTest {
                 .build());
     }
 
-    //특정 사용자의 투두 리스트 조회
+    /*
+     *    투두 리스트 조회
+     */
     @Test
     public void getTodoList() {
         todoRepository.findByUserId("test");
+    }
+
+    /*
+     *    투두 수정
+     */
+    @Test
+    public void putTodo() {
+        postTodo();
+        TodoDTO todoDto = TodoDTO.builder()
+                .id(1L)
+                .title("title- 수정")
+                .done(true)
+                .build();
+        TodoEntity todoEntity = todoRepository.findById(todoDto.getId())
+                .orElseThrow(() -> new CustomException("해당 투두가 없습니다."));
+        todoEntity.changeTitle(todoDto.getTitle());
+        todoEntity.changeDone(todoDto.isDone());
+    }
+
+    /*
+     *    투두 삭제
+     */
+    @Test
+    public void deleteTodo() {
+        postTodo();
+        todoRepository.deleteById(1L);
     }
 }
