541734b79f17395d90b35a3d8dce7153d183df3f
parksujin <mango4815@neighbor21.co.kr>
2022-10-24 15:48:46 +0900

    refactor : #12 - deprecated된 WebSecurityConfigurerAdapter refacor

--- src/main/java/com/web/springboot/config/WebSecurityConfig.java
+++ src/main/java/com/web/springboot/config/WebSecurityConfig.java
@@ -3,15 +3,16 @@ package com.web.springboot.config;
 import com.web.springboot.security.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @Slf4j
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
@@ -20,8 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // http 시큐리티 빌더
         http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
                 .and()
@@ -30,13 +31,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 기반이 아님을 선언
                 .and()
                 .authorizeRequests().antMatchers("/", "/auth/**").permitAll() // /와 /auth/** 경로는 인증 안해도 됨.
-                .anyRequest().authenticated(); // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+                .anyRequest().authenticated() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+                .and()
+                // filter 등록.
+                // 매 요청마다 CorsFilter 실행한 후에 jwtAuthenticationFilter 실행한다.
+                .addFilterBefore(jwtAuthenticationFilter, CorsFilter.class);
+        return http.build();
 
-        // filter 등록.
-        // 매 요청마다 CorsFilter 실행한 후에 jwtAuthenticationFilter 실행한다.
-        http.addFilterAfter(
-                jwtAuthenticationFilter,
-                CorsFilter.class
-        );
+
     }
 }
