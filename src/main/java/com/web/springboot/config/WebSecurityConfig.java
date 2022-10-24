b2342f0118f0e536bf758fb14ce224aeeb03029d
parksujin <mango4815@neighbor21.co.kr>
2022-10-24 11:35:49 +0900

    feat : #11 - WebSecurityConfig 파일 추가

---
+++ src/main/java/com/web/springboot/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.web.springboot.config;
+
+import com.web.springboot.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Slf4j
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http 시큐리티 빌더
+        http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+                .and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 기반이 아님을 선언
+                .and()
+                .authorizeRequests().antMatchers("/", "/auth/**").permitAll() // /와 /auth/** 경로는 인증 안해도 됨.
+                .anyRequest().authenticated(); // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+
+        // filter 등록.
+        // 매 요청마다 CorsFilter 실행한 후에 jwtAuthenticationFilter 실행한다.
+        http.addFilterAfter(
+                jwtAuthenticationFilter,
+                CorsFilter.class
+        );
+    }
+}
